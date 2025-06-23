a62304ebef8dd155c50b1d8fd868a97a5d378a63
uv0503 <picture0503@gmail.com>
2025-06-23 18:29:38 +0530

    Your commit message

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/home', (req, res) => {
+  res.send('Hello Janta!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
